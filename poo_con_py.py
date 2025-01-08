89a4c0e74234c034856efc19ebc7f4d04c2d6e4a
JoaquinCar <joaquincarmona758@gmail.com>
2025-01-08 11:27:30 -0600

    Clase 8/enero/Creando atributos de la clase

--- poo_con_py.py
+++ poo_con_py.py
@@ -1,1 +1,16 @@
-print("miss u betito")
+class personaje:
+    name = 'Default'
+    strenght = 0
+    intelligence = 0
+    defense = 0
+    hp = 0
+
+mi_personaje = personaje()
+mi_personaje.name = "Luis Vera"
+mi_personaje.strenght = 10
+mi_personaje.intelligence = 5
+mi_personaje.defense = 5
+mi_personaje.hp = 1
+
+print("El nombre del personaje es" ,mi_personaje.name)
+print("El nombre del personaje es" ,mi_personaje.strenght)
