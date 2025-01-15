f1a601dd492c1cfcf050a7139ae592a05b31210a
JoaquinCar <joaquincarmona758@gmail.com>
2025-01-15 11:10:15 -0600

    simulando_encapsulamiento

--- poo_con_py.py
+++ poo_con_py.py
@@ -33,19 +33,30 @@ class Personaje:
             print(f"{self.__name} attacked {enemy.__name} and caused {damage} of damage")
             print (f"{enemy.__name} has {enemy.__hp} points of life, and now is dead")
         else:
-            print(f"{self.__name} attacked {enemy.__name} and caused {damage} of damage")
-            print(f"{enemy.__name} life is {enemy.__hp}")
+            print(f"{self.__name} attacked {enemy.__name} and caused {damage} of damage /n {enemy.__name} life is {enemy.__hp}")
     
     def estarVivo(self):
         if self.__hp>0:
             print(f"{self.__name} is alive")
         else:
             print(f"{self.__name} is dead")
+    def get_strenght(self):
+        return f"la fuerza es {self.__strenght}"
+    
+    def set_strenght(self,strenght):
+        self.__strenght = strenght
+        if strenght <= 0:
+            self.__strenght = 0
+            print("No se puede tener fuerza negativa")
+        else:
+            return f"la nueva fuerza es {self.__strenght}"
 
 personaje1 = Personaje("Lalo", 10, 40,15,100)
 personaje2= Personaje ("Luis", 10, 30, 50, 100)
 #personaje1.subirNivel(10,5,5)
 #personaje1.atributos()
 #personaje1.estarVivo()
-personaje1.attack(personaje2)
-#personaje2.atributos()
+#personaje1.attack(personaje2)
+personaje1.set_strenght(-25)
+print(personaje1.get_strenght())
+
